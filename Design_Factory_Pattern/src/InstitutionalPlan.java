6b4e51aa28481cb91be37c2918b38f6e2e3a69b9
Gaurav Kumar Singh <singhkumargg999@gmail.com>
2021-06-13 22:15:56 +0530

    Factory Design Pattern

---
+++ Design_Factory_Pattern/src/InstitutionalPlan.java
@@ -0,0 +1,9 @@
+
+public class InstitutionalPlan extends Plan {
+
+	@Override
+	void getRate() {
+		 rate=7.50;
+	}
+
+}
---
+++ Design_Factory_Pattern/src/Plan.java
@@ -0,0 +1,14 @@
+
+abstract class Plan {
+	
+	protected double rate;
+	abstract void getRate();
+	
+	public void calculateBill(int units)
+	{
+		System.out.println(units*rate);
+	}
+}
+
+
+// created a plan abstract clas
